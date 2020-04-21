b540f622d0dfd15a2d3f79e5926ab6b92190fc62
Maria7744 <pola7744@mail.ru>
2020-04-21 15:20:59 +0300

    Quartz [#175122] Дополнительная новая задача

---
+++ chapter_001/src/main/java/teoria/AllertRabbit.java
@@ -0,0 +1,47 @@
+package teoria;
+import org.quartz.*;
+import org.quartz.impl.StdSchedulerFactory;
+import static org.quartz.JobBuilder.*;
+import static org.quartz.TriggerBuilder.*;
+import static org.quartz.SimpleScheduleBuilder.*;
+//Предположим, что нам нужно в консоль выводить сообщение раз в 10 секунд.
+public class AllertRabbit {
+    public static void main(String[] args) {
+        try {
+            //1. Конфигурирование.
+            //В объект Scheduler мы будем добавлять задачи, которые хотим выполнять периодически.
+            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+            scheduler.start();
+            //2. Создание задачи.
+            JobDetail job = newJob(Rabbit.class).build();
+           //3. Создание расписания.
+            //Конструкция  настраивает периодичность запуска. В нашем случае,
+            // мы будем запускать задачу через 10 секунд и делать это бесконечно.
+            SimpleScheduleBuilder times = simpleSchedule()
+                    .withIntervalInSeconds(10)
+                    .repeatForever();
+
+            // Задача выполняется через триггер.Здесь можно указать,
+            // когда начинать запуск. Мы хотим сделать это сразу.
+            Trigger trigger = newTrigger()
+                    .startNow()
+                    .withSchedule(times)
+                    .build();
+           //5. Загрузка задачи и триггера в планировщик
+            scheduler.scheduleJob(job, trigger);
+        } catch (SchedulerException se) {
+            se.printStackTrace();
+        }
+    }
+//quartz каждый раз создает объект с типом org.quartz.Job.
+// Вам нужно создать класс реализующий этот интерфейс.
+//
+//Внутри этого класса нужно описать требуемые действия. В нашем случае - это вывод на консоль текста.
+    public static class Rabbit implements Job {
+        @Override
+        public void execute(JobExecutionContext context) throws JobExecutionException {
+            System.out.println("Rabbit runs here ...");
+        }
+    }
+}
+
